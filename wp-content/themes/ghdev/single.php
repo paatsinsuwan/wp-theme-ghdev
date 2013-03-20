6aecca6fd25bdf3f524dc3c085d66b833a24c732
Paat Sinsuwan <paat@sinsuwan.com>
2013-03-20 16:29:49 -0400

    Change slideout on hover, fix nav links background on ie

--- wp-content/themes/ghdev/single.php
+++ wp-content/themes/ghdev/single.php
@@ -68,7 +68,7 @@ get_header(); ?>
 			            imageWidth: '40px',                       //width of tab image            //Optionally can be set using css
 			            tabLocation: 'right',                      //side of screen where tab lives, top, right, bottom, or left
 			            speed: 300,                               //speed of animation
-			            action: 'click',                          //options: 'click' or 'hover', action to trigger animation
+			            action: 'hover',                          //options: 'click' or 'hover', action to trigger animation
 			            topPos: '60px',                          //position from the top/ use if tabLocation is left or right
 			            leftPos: '20px',                          //position from left/ use if tabLocation is bottom or top
 			            fixedPosition: true                    //options: true makes it stick(fixed position) on scroll
